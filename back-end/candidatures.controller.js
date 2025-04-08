215af3219661081ec3e49b61a908b154e4513286
imane-glitch <imane.iguederzen76@gmail.com>
2025-04-08 10:42:33 +0200

    modif

--- back-end/candidatures.controller.js
+++ back-end/candidatures.controller.js
@@ -12,19 +12,32 @@ export const createCandidature = async (req,res) => {
     }
 }
 
-export const readCandidatures = (req,res) => {
-    //try {
-   //   const response = await ModelCandidature.find(req.body)
-    //} catch (error) {
-    //  res.status(200).json(error.message)
-   // }
+export const readCandidatures = async (req,res) => {
+  try {
+    const response = await ModelCandidature.find(req.body)
+    } catch (error) {
+    res.status(200).json(error.message)
+    }
 
 }
 
-export const deleteCandidature = (req,res) => { 
+export const deleteCandidature = async (req,res) => {
+  try {
+      const response = await ModelCandidature.findByIdAndDelete(req.params.id)
+      res.status(200).json({message: 'a été supprimé', response})
+    } catch (error) {
+      res.status(500).json(error.message)
+    }   
+  }
 
-}
 
-export const updateCandidature = (req,res) => {
+export const updateCandidature = async (req,res) => {
+  try {
+      const response = await ModelCandidature.findByIdAndUpdate(req.params.id, req.body)
+      res.status(200).json({message: 'a été modifié', response})
+    }
+    catch (error) {
+      res.status(500).json(error.message)
+    }
 
 }
